f47e77384763b857d66f67a3d8a6796ef2404af1
Catarina Rinaldi <catarinarinaldi@hotmail.com>
2021-12-17 19:29:59 -0300

    Versão Inicial do Projeto

---
+++ src/br/com/generation/projetojava/Diversifica_main.java
@@ -0,0 +1,59 @@
+package br.com.generation.projetojava;
+
+public class Diversifica_main {
+	 /*Main - diversiFica
+	 * Autore: catarina
+	 * Data: 16/12/2021
+	 */	
+	
+	public static void main(String[] args) {
+		
+//		CRIAR CONTA
+		
+//			EMPRESA
+			
+//				CADASTRO DA EMPRESA
+					
+//					INFOS BÁSICAS: FORMULÁRIO | SALVAR
+							
+//						OFERECER EMPREGO
+		
+//							SE SIM: PREENCHER INFOS DA VAGA | SALVAR
+//								OFERECER OUTRA VAGA
+							
+//							SE NÃO: VOLTAR PARA PÁGINA INICIAL
+			
+		
+//			PESSOA FÍSICA
+			
+//				CADASTRO
+					
+//					PREENCHER INFOS | SALVAR
+					
+//						PROCURANDO EMPREGO? SIM/NÃO
+							
+//							SIM: PREENCHER CV | SALVAR | VER LISTA DE EMPRESA > FILTRAR LISTA
+							
+//							NÃO: AVALIAR EMPRESA
+					
+//								PREENCHER O FORMULÁRIO DE AVALIAÇÃO | SALVAR
+
+//									VER LISTA DE EMPRESA > FILTRAR LISTA | VOLTAR PÁGINA INICIAL
+									
+		
+		
+//		LISTA DE EMPRESAS
+
+//			VER LISTA DE EMPRESA > FILTRAR LISTA			
+		
+		
+//		0 - RETORNA PARA PÁGINA INICIAL
+		
+//		9 - SALVAR
+		
+		
+		
+
+	}
+
+}
